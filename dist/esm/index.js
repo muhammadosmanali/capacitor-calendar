9904945c87cf58523b31e07d905facdf6229f315
Usman <muausman111@gmail.com>
2023-07-17 10:04:40 +0500

    add dist folder

---
+++ dist/esm/index.js
@@ -0,0 +1,5 @@
+import { registerPlugin } from '@capacitor/core';
+const CapacitorCalendar = registerPlugin('CapacitorCalendar', {});
+export * from './definitions';
+export { CapacitorCalendar };
+//# sourceMappingURL=index.js.map
